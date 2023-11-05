08ca62490215fe3695a8b2eb738726eac22f3356
Yash Bhatt <yash121@gmail.com>
2023-11-05 15:08:27 +0530

    hello

--- db/connect_db.js
+++ db/connect_db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const liveUrl = 'mongodb+srv://yash121bhatt:<yash123>@cluster0.nfxdvqy.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect('mongodb://127.0.0.1:27017/test');
+const liveUrl = 'mongodb+srv://yash121bhatt:yash123@cluster0.nfxdvqy.mongodb.net/admissionPortal?retryWrites=true&w=majority'
+const local_url ='mongodb://127.0.0.1:27017/admissionPortal'
+//  mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 
 
 const connect_db = () =>{
+    // return mongoose.connect('mongodb://127.0.0.1:27017/admissionPortal')
     return mongoose.connect(liveUrl)
     .then(()=>{
         console.log("Connected Succeessfully")
